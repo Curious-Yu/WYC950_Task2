0ae5de8139a91277e0b555cf8993e85e2c47c65d
WY <wyu10amy@hotmail.com>
2024-02-23 14:10:52 -0800

    Create hashtable.py

---
+++ hashtable.py
@@ -0,0 +1,61 @@
+# HashTable class using chaining
+# create HashTable data structure
+class ChainingHashTable:
+    # Constructor with optional initial capacity parameter.
+    # Assigns all buckets with an empty list.
+    def __init__(self, initial_capacity=39):
+        # initialize the hash table with empty bucket list entries.
+        self.table = []
+        for i in range(initial_capacity):
+            self.table.append([])
+
+    # Inserts a new item into the hash table.
+    def insert(self, key, item):  #  does both insert and update
+        # get the bucket list where this item will go.
+        # assuming key is a string coercible into int
+        bucket = int(key) % len(self.table)
+        bucket_list = self.table[bucket]
+
+        # update key if it is already in the bucket
+        for kv in bucket_list:
+            # print (key_value)
+            if kv[0] == key:
+                kv[1] = item
+                return True
+
+        # if not, insert the item to the end of the bucket list.
+        key_value = [key, item]
+        bucket_list.append(key_value)
+        return True
+
+    # Searches for an item with matching key in the hash table.
+    # Returns the item if found, or None if not found.
+
+    def search(self, key):
+        # get the bucket list where this key would be.
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+        # print(bucket_list)
+
+        # search for the key in the bucket list
+        for kv in bucket_list:
+            # print (key_value)
+            if kv[0] == key:
+                return kv[1]  # value
+        return None
+
+    # Removes an item with matching key from the hash table.
+
+    def remove(self, key):
+        # get the bucket list where this item will be removed from.
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+
+        # remove the item from the bucket list if it is present.
+        for kv in bucket_list:
+            # print (key_value)
+            if kv[0] == key:
+                # bucket_list.remove([kv[0], kv[1]])
+                bucket_list.remove(kv)
+                return True
+        return False  # key not found
