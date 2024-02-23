2b98f1858c3fa49187c21a86410ba1b2b99fdafc
WY <wyu10amy@hotmail.com>
2024-02-23 14:47:20 -0800

    added comment

--- truck.py
+++ truck.py
@@ -1,16 +1,20 @@
+# import the csv module to work with csv files
 import csv
 
+# import datetime and timedelta classes
 from datetime import datetime, timedelta
 
+# import the chaining hash table class from hashtable module
 from hashtable import ChainingHashTable
+
+# import package class and load package data function from package module
 from package import Package, load_package_data
+
+# import load address data function from address module
 from address import load_address_data
+
+# import load distance data function from distance module
 from distance import load_distance_data
-
-
-from rich import print
-
-# delete before submit the assign -  this is only for readability
 
 
 # create truck object
@@ -26,6 +30,7 @@ class Truck:
         start_time,
         sorted_pkgs,
     ):
+        # initializing Truck object with provided attributes
         self.capacity = capacity
         self.speed = speed
         self.load = load
@@ -34,9 +39,12 @@ class Truck:
         self.start_address = start_address
         self.start_time = start_time
         self.time = start_time
-        self.sorted_pkgs: list[Package] = sorted_pkgs
+        self.sorted_pkgs: list[Package] = (
+            sorted_pkgs  # assume sorted pkgs is a list of Package objects
+        )
 
     def __str__(self):
+        # string representation of Truck object
         return "%s, %s, %s, %s, %s, %s, %s" % (
             self.capacity,
             self.speed,
