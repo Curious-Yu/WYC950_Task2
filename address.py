ba22cf3ea3822c85f0d9c38e65b7e3a731725d10
WY <wyu10amy@hotmail.com>
2024-02-23 14:33:32 -0800

    Update address.py

--- address.py
+++ address.py
@@ -1,9 +1,13 @@
-import csv
+import csv  # importing the csv module to work with csv files
 
 
-# load the address data
+# function to load address data from csv file
 def load_address_data():
+    # open the address csv file in read mode
     with open("excel/address.csv", "r") as address_file:
+        # use csv reader to read the content of the csv file
         address_data = csv.reader(address_file)
+        # converting the csv data into a list
         address_data = list(address_data)
+    # return the loaded address data
     return address_data
